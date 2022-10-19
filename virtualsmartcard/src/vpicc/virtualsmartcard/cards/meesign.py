3117dcd0efcbc41905225f302b2bc8835c0221f7
Kristian Mika <kristian.mika@protonmail.com>
2022-10-20 00:03:07 +0200

    card-init works

--- virtualsmartcard/src/vpicc/virtualsmartcard/cards/meesign.py
+++ virtualsmartcard/src/vpicc/virtualsmartcard/cards/meesign.py
@@ -1,5 +1,4 @@
 from multiprocessing.sharedctypes import Value
-from xmlrpc.client import Boolean
 import grpc
 from enum import Enum
 import struct
@@ -7,7 +6,6 @@ from datetime import datetime
 import time
 from Crypto.PublicKey import ECC
 
-from virtualsmartcard.utils import C_APDU
 from virtualsmartcard.SmartcardFilesystem import MF
 from virtualsmartcard.SEutils import Security_Environment
 from virtualsmartcard.SmartcardSAM import SAM
@@ -15,7 +13,7 @@ from virtualsmartcard.VirtualSmartcard import Iso7816OS
 from virtualsmartcard.SWutils import SwError, SW
 from virtualsmartcard.cards.mpc_pb2 import SignRequest, TaskRequest, Task
 from virtualsmartcard.cards.mpc_pb2_grpc import MPCStub
-from virtualsmartcard.ConstantDefinitions import MAX_EXTENDED_LE
+from virtualsmartcard.ConstantDefinitions import MAX_SHORT_LE
 
 
 class MeesignOS(Iso7816OS):
@@ -27,28 +25,32 @@ class MeesignOS(Iso7816OS):
     INFINIT_EID_ATR = bytes([0x3b, 0xfe, 0x18, 0x00, 0x00, 0x80, 0x31, 0xfe, 0x45, 0x80, 0x31,
                              0x80, 0x66, 0x40, 0x90, 0xa5, 0x10, 0x2e, 0x10, 0x83, 0x01, 0x90, 0x00, 0xf2])
 
-    def __init__(self, mf, sam, _meesign_url, _group_id,  ins2handler=None, maxle=MAX_EXTENDED_LE):
+    def __init__(self, mf, sam, _meesign_url, _group_id,  ins2handler=None, maxle=MAX_SHORT_LE):
         if not _group_id:
             raise ValueError("group_id not specified")
         Iso7816OS.__init__(self, mf, sam, ins2handler, maxle)
         self.ins2handler = {
+            0x01: self.SAM.generate_keypair,
             0x02: self.SAM.get_public_key,
             0x03: self.SAM.store_certificate,
             0x04: self.SAM.get_certificate,
             0x20: self.SAM.verify,
+            0x22: self.SAM.manage_security_environment,
             0x26: self.SAM.retries_left,
             0x2A: self.SAM.perform_security_operation,
             0x88: self.SAM.authenticate,
-            0xa4: self.mf.selectFile
+            0xa4: self.mf.selectFile,
+            0xb0: self.mf.readBinaryPlain, # TODO improve
+            0xc0: self.getResponse
         }
         self.atr = MeesignOS.INFINIT_EID_ATR
 
         sam.meesign_url = _meesign_url
-        sam.group_id = _group_id
-        sam.auth_pubkey = _group_id
+        group_id = bytes.fromhex(_group_id)
+        sam.group_id =  group_id
+        sam.auth_pubkey = group_id
+        mf.auth_cert = []
 
-    def execute(self, msg):
-        return Iso7816OS.execute(self, msg)
 
 
 class MeesignSE(Security_Environment):
@@ -56,12 +58,47 @@ class MeesignSE(Security_Environment):
 
 
 class MeesignMF(MF):
+
     AID = bytes([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08])
+        
+
     def selectFile(self, p1, p2, data):
         if data == MeesignMF.AID:
             return SW["NORMAL"], b""
-        else:
+        # self.append(TransparentStructureEF(parent=self, fid=FileType.FID_3F00.value,
+                    #    shortfid=0x1c, data=self.auth_cert))
+
+        # return MF.selectFile(self, p1, p2, data)
+
+
+        # TODO: use the ducking filesystem implementation
+        # if p1 == 0x00:
+        self.selected_file = FileType.FID_3F00
+        # else:
+            # return SW["ERR_FILENOTFOUND"], b""
+            # self.selected_file = FileType.FID_DDCE #TODO: completely wrong
+
+
+
+        return SW["NORMAL"], b""
+        
+
+
+    def readBinaryPlain(self, p1, p2, data):
+        file = None
+        offset = format_short(bytes([p1, p2]))
+
+
+        if self.selected_file == FileType.FID_3F00:
+            file = self.auth_cert[offset:min(offset + 0x80,len(self.auth_cert) )]
+        elif self.selected_file == FileType.FID_DDCE:
             return SW["ERR_FILENOTFOUND"], b""
+            
+        else:
+            print("not found file:", self.selected_file)
+            return SW["ERR_FILENOTFOUND"], b""
+
+        return SW["NORMAL"], file
 
 
 
@@ -88,11 +125,7 @@ class MeesignSAM(SAM):
         if len(data) > Pin.PIN_MAX_SIZE:
             raise SwError(SW["SW_WRONG_LENGTH"])
 
-        requested_pin_type = p2
-
-        req_pin = self.pins.get(requested_pin_type)
-        if req_pin is None:
-            raise SwError(SW["ERR_INCORRECTP1P2"])
+        req_pin = self.__get_pin(p2)
 
         if req_pin.attempts_left() == 0:
             raise SwError(SW["SW_PIN_BLOCKED"])
@@ -109,10 +142,7 @@ class MeesignSAM(SAM):
         if p1 != 0x00:
             raise SwError(SW["ERR_INCORRECTP1P2"])
 
-        requested_pin_type = p2
-        req_pin = self.pins.get(requested_pin_type)
-        if req_pin is None:
-            raise SwError(SW["ERR_INCORRECTP1P2"])
+        req_pin = self.__get_pin(p2)
 
         return SW["NORMAL"], format_unsigned_short(req_pin.attempts_left())
 
@@ -126,10 +156,12 @@ class MeesignSAM(SAM):
 
     def get_certificate(self, p1, p2, data):
         if p1 == 0x01:
-            return SW["NORMAL"], self.auth_cert
+            return SW["NORMAL"], self.mf.auth_cert
         elif p1 == 0x02:
-            # TODO: implement signing?
             raise SwError(SW["ERR_NOTSUPPORTED"])
+
+            # TODO: implement signing?
+            return SW["NORMAL"], self.mf.signing_cert
         else:
             raise SwError(SW["ERR_INCORRECTP1P2"])
 
@@ -138,30 +170,67 @@ class MeesignSAM(SAM):
             raise SwError(CustomSW.SW_PIN_VERIFICATION_REQUIRED)
 
         if p1 == 0x01:
-            # TODO: data == cert?
-            self.auth_cert = data
+            assert(self.mf.auth_cert != data)
+            self.mf.auth_cert += data
+
         elif p1 == 0x02:
             raise SwError(SW["ERR_NOTSUPPORTED"])
+            print("Storing signing cert")
+
+            self.mf.signing_cert += data
         else:
             raise SwError(SW["ERR_INCORRECTP1P2"])
 
         self.pins.get(PinType.ADMIN_PIN_REFERENCE).reset()
+        return SW["NORMAL"], b""
 
     def get_public_key(self, p1, p2, data):
         key = None
-        if p1 == Reference.AUTH_KEYPAIR_REFERENCE:
+        if p1 == Reference.AUTH_KEYPAIR_REFERENCE.value:
             key = self.auth_pubkey #TODO: store keys in a struct
-        elif p1 == Reference.SIGNING_KEYPAIR_REFERENCE:
+        elif p1 == Reference.SIGNING_KEYPAIR_REFERENCE.value:
+            key = self.auth_pubkey #TODO: wrong
+
+            # raise SwError(SW["ERR_NOTSUPPORTED"])
+
+            # key = self.signing_pubkey
             # TODO: implement signing?
-            raise SwError(SW["ERR_NOTSUPPORTED"])
         else:
             # TODO: not in the original applet
+            print("weird p1")
             raise SwError(SW["ERR_INCORRECTP1P2"])
 
 
-        if p2 != Reference.GET_PUBLIC_KEY_REFERENCE:
+        if p2 != Reference.GET_PUBLIC_KEY_REFERENCE.value:
+            print("p2 not 09")
             raise SwError(SW["ERR_INCORRECTP1P2"])
         return SW["NORMAL"], key
+
+    def manage_security_environment(self, p1, p2, data):
+        """ 
+        Set Pin
+        """
+        if p1 != 0x00:
+            raise SwError(SW["ERR_INCORRECTP1P2"])
+
+        pin = self.__get_pin(p2)
+
+        admin_pin = self.pins.get(PinType.ADMIN_PIN_REFERENCE)
+        if admin_pin.is_set() and not admin_pin.is_validated():
+            raise SwError(SW["ERR_SECSTATUS"])
+        pin.set_pin(data)
+        # pin.resetAndUnblock(); TODO
+        admin_pin.reset()
+        if not admin_pin.is_set() and p2 == PinType.ADMIN_PIN_REFERENCE:
+            admin_pin._is_set = True
+        return SW["NORMAL"], b""        
+
+    def generate_keypair(self, p1, p2, data):
+        if not self.pins.get(PinType.ADMIN_PIN_REFERENCE).is_validated():
+            raise SwError(SW["ERR_SECSTATUS"]) # TODO: SW_PIN_VERIFICATION_REQUIRED
+        # missing impl
+        return SW["NORMAL"], b""        
+
 
 
     def authenticate(self, p1, p2, data):
@@ -177,6 +246,12 @@ class MeesignSAM(SAM):
         self.pins.get(PinType.AUTH_PIN_REFERENCE).reset()
         return SW["NORMAL"], task.data # TODO: task.data?
 
+    def __get_pin(self, pin_type: int):
+        pintype = PinType(pin_type)
+        if not pintype:
+            raise SwError(SW["ERR_INCORRECTP1P2"])
+        return self.pins.get(pintype)
+        
     def __create_task(self, name: str, group_id: bytes, data: bytes):
         """
         Creates a new signing task
@@ -220,8 +295,10 @@ class Pin:
 
     def __init__(self):
         self.attempts = Pin.DEFAULT_ATEMPT_COUNT
+        self._is_set = False
 
     def set_pin(self, new_pin: bytes):
+        self._is_set = True
         pass
 
     def change_pin(self, new_pin: bytes):
@@ -245,35 +322,12 @@ class Pin:
     def is_validated(self) -> bool:
         return True
 
-    def reset():
+    def reset(self):
         pass
 
+    def is_set(self):
+        return self._is_set
 
-class InstructionType(Enum):
-    """ 
-    Holds instruction constants
-    """
-    INS_CHANGE_PIN = 0x24
-    INS_UNBLOCK = 0x2C
-    INS_RESET_RETRY_COUNTER = 0x2C
-    INS_SELECT = 0xA4
-    INS_READ_BINARY = 0xB0
-    INS_UPDATE_BINARY = 0xD6
-    INS_ERASE_BINARY = 0x0E
-    INS_READ_RECORD = 0xB2
-    INS_MANAGE_SECURITY_ENVIRONMENT = 0x22
-    INS_AUTHENTICATE = 0x88
-    INS_MUTUAL_AUTHENTICATE = 0x82
-    INS_GET_CHALLENGE = 0x84
-    INS_UPDATE_RECORD = 0xDC
-    INS_APPEND_RECORD = 0xE2
-    INS_GET_DATA = 0xCA
-    INS_PUT_DATA = 0xDA
-    INS_CREATE_FILE = 0xE0
-    INS_DELETE_FILE = 0xE4
-    INS_GENERATE_KEYPAIR = 0x01
-    INS_GET_RESPONSE = 0xC0
-    INS_SET_PIN = 0x22
 
 
 class PinType(Enum):
@@ -296,3 +350,11 @@ def format_unsigned_short(num):
     Serializes an unsigned short using small endian
     """
     return struct.pack(">H", num)
+
+def format_short(nums):
+    return struct.unpack(">H", nums)[0]
+
+class FileType(Enum):
+    FID_3F00 = 0x3F00
+    FID_AACE = 0xAACE
+    FID_DDCE = 0xDDCE
